8641a43528fcc96709cc31c9129f76ef7aea64b5
Stéphane Burwash <sburwash@live.ca>
2025-04-15 08:21:56 -0400

    Added document splitter

--- article_bot/main.py
+++ article_bot/main.py
@@ -1,5 +1,10 @@
 from langchain_community.tools import DuckDuckGoSearchResults
 from langchain_community.utilities.duckduckgo_search import DuckDuckGoSearchAPIWrapper
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+import bs4
 import re
 from dotenv import load_dotenv
 import os
@@ -22,3 +27,10 @@ def get_links(keyword):
         links.append(link)
         
     return links
+
+bs4_strainer = bs4.SoupStrainer(('p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'))
+document_loader = WebBaseLoader(web_path=(get_links(keyword)))
+docs = document_loader.load()
+splitter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200,add_start_index=True,)
+splits = splitter.split_documents(docs)
+vector_store = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
